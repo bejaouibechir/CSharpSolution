94545291b3d8d9201c2b6d40a4a33495a303edd8
bechirbejaoui <me780411@gmail.com>
2023-04-04 14:01:41 +0200

    040420231401

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,22 +4,17 @@ using System.Threading;
 namespace ConsoleApp
 {
     internal class Program
-    {
-        
-        static void Main(string[] args)
+    { 
+       static void Main(string[] args)
        {
-             int a = 2, b = 3;
-             Swap(a, b);
-                
-        }
-        static void Swap(int a, int b)
-        {
-            a = a + b;
-            b = a - b;
-            a = a - b;
-        }
+           
+
+       }    
     }
-      
+     
+    
+
+
 
 }
 
