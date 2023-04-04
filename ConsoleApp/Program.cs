2f04183c5f300ae0e9f419cedf1c596e9fb958a5
bechirbejaoui <me780411@gmail.com>
2023-04-04 17:10:21 +0200

    040420231710

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,112 +1,20 @@
 ﻿using System;
+using System.Configuration;
 
 namespace ConsoleApp
 {
-    internal class Program
-    { 
-       static void Main(string[] args)
-       {
-            Fahrenheit fahrenheit = new Fahrenheit();
-            fahrenheit.Value = 150.15f;
-
-            float valeur = System.Convert.ToSingle(fahrenheit);
-        }    
-    }
-
-    public struct Fahrenheit :IConvertible 
+    class program
     {
-        public float Value;
-
-        public TypeCode GetTypeCode()
+        static void Main(string[] args)
         {
-            throw new NotImplementedException();
-        }
+           
 
-        public bool ToBoolean(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
         }
-
-        public byte ToByte(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public char ToChar(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public DateTime ToDateTime(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public decimal ToDecimal(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public double ToDouble(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public short ToInt16(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int ToInt32(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public long ToInt64(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public sbyte ToSByte(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public float ToSingle(IFormatProvider provider)
-        {
-            return Value;
-        }
-
-        public string ToString(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public object ToType(Type conversionType, IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ushort ToUInt16(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public uint ToUInt32(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ulong ToUInt64(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
+         //Heritage  encapsulation et polymorphisme de classe POO
     }
-    //(32°F − 32) × 5/9 
-    public struct Celsius
-    {
-        public float Value;
-    }
+
+  
+
 }
 
 
