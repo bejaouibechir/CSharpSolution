86762ffd19a4e6633dff1dd0e1c661bf27ca3aab
bechirbejaoui <me780411@gmail.com>
2023-04-04 13:25:00 +0200

    040420231324

---
+++ WindowsForm/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Gestionnaire d'événement
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"La date actuelle est {DateTime.Now}");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Le jour est {DateTime.Now.DayOfWeek}");
+        }
+    }
+}
