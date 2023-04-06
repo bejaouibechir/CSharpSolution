44983b7b0581d060f3f8d04d3a25b3d09a39853a
bechirbejaoui <me780411@gmail.com>
2023-04-06 10:35:09 +0200

    060420231035

--- DataAdoProject/CRUD/DataContext.cs
+++ DataAdoProject/CRUD/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.OleDb;
 using System.Data.SqlClient;
 using System.Diagnostics;
@@ -27,7 +28,7 @@ namespace DataAdoProject.CRUD
         {
             try
             {
-                _sql = $"INSERT INTO [dbo].[Categories]([Id],[Label]) VALUES({categorie.Id},{categorie.Label})";
+                _sql = $"INSERT INTO [dbo].[Categories]([Id],[Label]) VALUES({categorie.Id},'{categorie.Label}')";
                 _command = new SqlCommand(_sql, _connection);
                 _connection.Open();
                 _command.ExecuteNonQuery();
@@ -42,6 +43,114 @@ namespace DataAdoProject.CRUD
             }
 
         }
+        public void UpdateCategorie(int id, string label)
+        {
+            try
+            {
+                _sql = $"UPDATE [dbo].[Categories] SET [Label] ='{label}'  WHERE Id ={id}";
+                _command = new SqlCommand(_sql, _connection);
+                _connection.Open();
+                _command.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+        }
+        public void DeleteCategorie(int id)
+        {
+            try
+            {
+                _sql = $"DELETE FROM [dbo].[Categories] WHERE Id={id}";
+                _command = new SqlCommand(_sql, _connection);
+                _connection.Open();
+                _command.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+        }
+
+        public Categorie GetCategorie(int id)
+        {
+            Categorie categorie;
+            try
+            {
+                _sql = $"SELECT [Id],[Label] FROM [dbo].[Categories] WHERE Id = {id}";
+                _command = new SqlCommand(_sql, _connection);
+                _connection.Open();
+                _reader = _command.ExecuteReader(System.Data.CommandBehavior.SingleRow);
+                _reader.Read();
+                
+                categorie = new Categorie
+                {
+                    Id = int.Parse(_reader[0].ToString()),
+                    Label = _reader[1].ToString(),
+                };
+
+                return categorie;
+                
+            }
+            catch (SqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+
+        }
+
+
+        public List<Categorie> GetCategorieList()
+        {
+            List<Categorie> categorieList;
+            Categorie categorie;
+            try
+            {
+                _sql = $"SELECT [Id],[Label] FROM [dbo].[Categories]";
+                _command = new SqlCommand(_sql, _connection);
+                _connection.Open();
+                _reader = _command.ExecuteReader();
+                categorieList = new List<Categorie>();
+                while (_reader.Read())
+                {
+                    categorie = new Categorie
+                    {
+                        Id = int.Parse(_reader[0].ToString()),
+                        Label = _reader[1].ToString()
+                    };
+                    categorieList.Add(categorie);
+                }
+                return categorieList;
+
+            }
+            catch (SqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+        }
+
 
     }
 }
--- DataAdoProject/Program.cs
+++ DataAdoProject/Program.cs
@@ -1,15 +1,70 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DataAdoProject.CRUD;
 
 namespace DataAdoProject
 {
     internal class Program
     {
+        static DataContext _context;
+      
         static void Main(string[] args)
         {
+            _context = new DataContext();
+            List<Categorie> categorie = _context.GetCategorieList();
+            Console.Read();
         }
     }
 }
+
+
+
+/*
+ // Test d'ajout de categorie
+
+ _context = new DataContext();
+
+            Categorie categorie1 = new Categorie { Id = 1, Label = "Voitures" };
+            Categorie categorie2 = new Categorie { Id = 2, Label = "Motos" };
+            Categorie categorie3 = new Categorie { Id = 3, Label = "Camions" };
+
+            _context.AddCategorie(categorie1);
+            _context.AddCategorie(categorie2);
+            _context.AddCategorie(categorie3);
+
+            Console.WriteLine("Les trois catégories sont ajoutées");
+            Console.Read();
+ 
+ 
+ 
+ */
+
+/*
+ //Test de mise à jour de catégorie
+ _context = new DataContext();
+            _context.UpdateCategorie(2, "Motocycles");
+            Console.WriteLine("Une mise à jour a été effectuée");
+            Console.Read();
+ */
+
+/*
+ //Test de supression de catégorie
+
+ _context = new DataContext();
+            _context.DeleteCategorie(3);
+            Console.WriteLine("Une supression a été effectuée");
+            Console.Read();
+ 
+ */
+
+/*
+ //Test de recuperation de categorie
+
+_context = new DataContext();
+            Categorie categorie = _context.GetCategorie(1);
+            Console.Read();
+ 
+ 
+ */
